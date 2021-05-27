fdb51b7f6dd2c61d2638a83fafcfb2f88d2ec4e1
Marius-Mihail Gurgu <32803079+TheLonelyFighter@users.noreply.github.com>
2021-05-27 21:50:49 +0200

    Update gen_round_keyz_tb.v

--- gen_round_keyz_tb.v
+++ gen_round_keyz_tb.v
@@ -23,31 +23,33 @@
 `include "gen_round_keyz.v"
 module generateRoundKeys_tb();
 reg[79:0] key;
-reg[7:0] rounds;
+reg[5:0] round_counter;
 reg clock;
+reg enable_in;
 wire[63:0] round_out;
 wire[79:0] aux;
-reg[63:0] just_count;
 
 initial begin
-    $display ("time    Round    aux    round_out");	
-    $monitor ("%g\t %d    %h    %h",   $time, just_count, aux, round_out);	
+    $display ("time    Round       round_out");	
+    $monitor ("%g\t %d     %h",   $time, round_counter,  round_out);	
     clock = 0;
-    rounds = 32;
+    round_counter = -1;
     key = 80'h00000000000000000000;
-    just_count = 0;
+    enable_in = 0;
+    round_counter = round_counter + 1;
+    #100 enable_in = 1;
 end
 
 always begin
-#50 clock = ~clock;
-    just_count = just_count + 1;
+#200 clock = ~clock;    
+    round_counter = round_counter + 1;
 end
 
 gen_round_keyz U_generate(
 key,
-rounds,
 clock,
-round_out,
-aux
+enable_in,
+round_counter,
+round_out
 );
 endmodule
